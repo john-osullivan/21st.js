d365ad0b087059e1655d540d0e0903cb9f65f682
John O'Sullivan <j.osullivan42@gmail.com>
2016-05-22 03:08:15 -0400

    Got skeleton to stop erroring, removed double setting of Account variable.

--- src/QuizApp/client/components/QuizApp.jsx
+++ src/QuizApp/client/components/QuizApp.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Component } from 'react';
+import ReactMixin from 'react-mixin';
 
 import QuizQuestions from './QuizQuestions';
 import QuizSetup from './QuizSetup.jsx';
 
+@ReactMixin.decorate(ReactMeteorData)
 export default class QuizMain extends Component {
     state = {
+        currentTurn : 'start'
+    };
 
-    }
     render(){
-
+        return (
+            <div className='container ui grid'>
+                <div className='twelve wide column'>
+                    <GameLogin />
+                    <QuizQuestions />
+                </div>
+                <div className='four wide column'>
+                    <QuizSetup />
+                </div>
+            </div>
+        );
     }
 }
